2bc3b900ebc125f3ec04a6c3e6596deaddab8b3a
Nathan <nathan.janeczko@vivoka.com>
2018-03-02 12:09:59 +0100

    Add a list of card with there weight

---
+++ list_card.py
@@ -0,0 +1,10 @@
+weight_data = {
+    "rouge": 8, # Pioche suspect ou fantome
+    "gris": 7, # Eteint une pièce
+    "noir": 6, # Attire adjacents
+    "blanc": 5, # Repousse
+    "violet": 4, # Switch
+    "marron": 3, # Kidnap
+    "bleu": 2, # Bloque un passages
+    "rose": 1 # Passage secret
+}
